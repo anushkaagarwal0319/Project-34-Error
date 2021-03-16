9bbac7770436faf49f48d7536920368c58b1310f
anushkaagarwal0319 <72772476+anushkaagarwal0319@users.noreply.github.com>
2021-03-16 16:24:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+
+function preload() {
+//preload the images here
+
+bg = loadImage("images/sky.jpg");
+
+}
+
+function setup() {
+  createCanvas(1000, 600);
+
+	engine = Engine.create();
+	world = engine.world;
+
+  Engine.run(engine);
+
+  //hero = new Hero(400, 100, 100);
+  ground = new Ground(300, 350, 800, 5);
+
+  fly = new Fly(hero.body, {x:400, y:30});
+
+}
+
+function draw() {
+  background(bg);
+
+  //hero.display();
+  ground.display();
+
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(hero.body, {x:mouseX, y:mouseY});
+}
